17f7c0b69040b686e5fa287341e239147ad5a70d
Tushar Hatwar <60131764+tushar-hatwar@users.noreply.github.com>
2023-06-08 17:31:47 +0530

    removed client secret mount_Adls.py

--- ticket_data_poc/mount_Adls.py
+++ ticket_data_poc/mount_Adls.py
@@ -2,7 +2,7 @@
 storage_account_name = "saticketpocdata08062023"
 client_id            = "0aad77b6-a9ff-45e3-a931-c16111e79468"
 tenant_id            = "a9a1ad60-0851-40df-8e61-d9dd36cf0e8b"
-client_secret        = "TA_8Q~MKwv1rOtO_SAcWPDIU-NEfzQxF2~Kzsbd~"
+client_secret        = ""
 
 # COMMAND ----------
 
